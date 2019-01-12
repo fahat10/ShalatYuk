68dff59ef1fb1af8aaae8ad4f7b98a351a99b667
fahat10 <46435624+fahat10@users.noreply.github.com>
2019-01-12 13:42:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -83,7 +83,7 @@ if ($type == 'join' || $command == 'Hallo') {
 
 //pesan bergambar
 if($message['type']=='text') {
-	    if ($command == 'jadwal') {
+	    if ($command == '/shalat') {
 
         $result = shalat($options);
         $balas = array(
